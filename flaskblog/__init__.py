8ae845406021367e6ed990772d4a658eb618cdcf
discomax <mpatterson73@gmail.com>
2019-07-03 11:06:27 -0400

    file reorg

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e3d415735ac112a2262ff99a9b3680c0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
