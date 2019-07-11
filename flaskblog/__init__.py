4c37372c94b42e4b3fd1b7a7df63637ef544b63a
discomax <mpatterson73@gmail.com>
2019-07-11 17:36:39 -0400

    update config file

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -8,18 +8,13 @@ from flask_mail import Mail
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'e3d415735ac112a2262ff99a9b3680c0'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'mpatterson73@gmail.com'
-app.config['MAIL_PASSWORD'] = 'reagan7705'
+
 mail = Mail(app)
 
 from flaskblog.users.routes import users  # user instance with the user blueprint
---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class config:
+    SECRET_KEY = 'e3d415735ac112a2262ff99a9b3680c0'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 
+    MAIL_PASSWORD = 
