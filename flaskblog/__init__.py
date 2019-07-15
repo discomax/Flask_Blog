c80ea028e27562b0e6068551f2f0912239a79103
discomax <mpatterson73@gmail.com>
2019-07-15 12:36:50 -0400

    create instances using function

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -7,20 +7,29 @@ from flask_mail import Mail
 from flaskblog.config import Config
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-mail = Mail(app)
+mail = Mail()
 
-from flaskblog.users.routes import users  # user instance with the user blueprint
-from flaskblog.posts.routes import posts  # posts instance with the posts blueprint
-from flaskblog.main.routes import main  # main instance with the main blueprint
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.main.routes import main  # main instance with the main blueprint
+    from flaskblog.users.routes import users  # user instance with the user blueprint
+    from flaskblog.posts.routes import posts  # posts instance with the posts blueprint
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
